e6f6ef3909883b44062b0db046849ae00f97ccc8
PhilAnggara <bawoleangga03@gmail.com>
2026-02-26 15:15:54 +0800

    simplifying database connection settings

--- config/database.php
+++ config/database.php
@@ -14,9 +14,17 @@ return [
     | the connection which will be utilized unless another connection
     | is explicitly specified when you execute a query / statement.
     |
+    | DB_PROFILE options:
+    |   1 = Home (local MySQL)
+    |   2 = Office (SQL Server)
+    |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => match (env('DB_PROFILE', '1')) {
+        '2' => 'sqlsrv',  // Office - SQL Server
+        '1' => 'mysql',   // Home - MySQL
+        default => env('DB_CONNECTION', 'sqlite'),
+    },
 
     /*
     |--------------------------------------------------------------------------
@@ -46,11 +54,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_MYSQL_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('DB_MYSQL_PORT', env('DB_PORT', '3306')),
+            'database' => env('DB_MYSQL_DATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('DB_MYSQL_USERNAME', env('DB_USERNAME', 'root')),
+            'password' => env('DB_MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -101,16 +109,16 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_SQLSRV_HOST', env('DB_HOST', 'localhost')),
+            'port' => env('DB_SQLSRV_PORT', env('DB_PORT', '1433')),
+            'database' => env('DB_SQLSRV_DATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('DB_SQLSRV_USERNAME', env('DB_USERNAME', 'sa')),
+            'password' => env('DB_SQLSRV_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_SQLSRV_ENCRYPT', env('DB_ENCRYPT', 'yes')),
+            'trust_server_certificate' => env('DB_SQLSRV_TRUST_SERVER_CERTIFICATE', env('DB_TRUST_SERVER_CERTIFICATE', 'false')),
         ],
 
         // Legacy DB koneksi 1 (mis. sistem lama utama).
